8012d8e8c3eefdaf60970734b26008656fa151ab
GeantW0rld <yanis.geantworld@gmail.com>
2023-08-02 20:29:32 +0200

    updating

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(urlencodedParser)
 const oauth = new DiscordOauth2({
   clientId: config.clientId,
   clientSecret: config.clientSecret,
-  redirectUri: `${config.debugdomain}/callback`
+  redirectUri: `${config.domain}/callback`
 })
 
 client.commands = new Collection()
