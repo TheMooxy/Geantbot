d25b4a70dafdb5ccb93f83bc69941ec5f562b33d
GeantW0rld <yanis.geantworld@gmail.com>
2023-08-02 20:21:07 +0200

    delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const {
-    ButtonBuilder,
-    ButtonStyle,
-    ActionRowBuilder
-} = require("discord.js");
-
-interaction.reply({content: "ah shit bro, here we go again", components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("hi").setLabel("bouncing balls").SetStyle(ButtonStyle.Danger))]})
